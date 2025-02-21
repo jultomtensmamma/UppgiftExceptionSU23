14fa8d40c5ae007d58f1d83a5b5efb0e8529af89
jultomtensmamma <julia.tillberg@elev.ga.lbs.se>
2025-02-21 12:41:17 +0100

    skrivit kod

--- UppgiftExceptions/Program.cs
+++ UppgiftExceptions/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
 class Program
 {
     static void Main(string[] args)
     {
         //Del 1
+        try
+        {
             Console.WriteLine("Ange ett tal:");
             int tal1 = int.Parse(Console.ReadLine());
 
@@ -13,6 +16,15 @@ class Program
 
             int resultat = tal1 / tal2;  // Möjlig division med noll
             Console.WriteLine($"Resultat: {resultat}");
+        }
+        catch (FormatException ex)
+        {
+            Console.Writeline("Ogiltig inmatning, vänligen ange ett heltal")
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Ett fel uppstod" + ex.Message);
+        }
 
         //Möjliga fel:
         //Att använderaen inte matar in ett tal
@@ -21,14 +33,29 @@ class Program
 
         /*------------------------------------------------------------------------------------------*/
         //Del 2
-        Console.WriteLine("Ange sidolängden på en kub:");
-        int sida = int.Parse(Console.ReadLine());
+        public class MyOwnException: Exception
+    {
+        public MyOwnException() : base() { }
+        public MyOwnException(string message) : base(message) { }
+        public MyOwnException (string message, Exception innerExeption) : base(message, innerException) { }
 
-        Console.WriteLine("Volymen blir: " + (Math.Pow(sida,3)));
-        //Möjliga fel att hantera
-        //Användaren försöker mata in negativa värden. 
+    }
+        try
+        {
+            Console.WriteLine("Ange sidolängden på en kub:");
+            int sida = int.Parse(Console.ReadLine());
+             if (sida < 0)            
+                   throw new MyOwnException(" ")
 
-        
+            Console.WriteLine("Volymen blir: " + (Math.Pow(sida, 3)));
+            //Möjliga fel att hantera
+            //Användaren försöker mata in negativa värden. 
+        }
+        catch (MyOwnException)
+        {
+    
+        }
+
         /*------------------------------------------------------------------------------------------*/
         //Del 3
         // Kom på en egen uppgift där du behöver skapa din egna Exception. Skriv koden nedan
